52e536e8361ea4e704a4dcdb5d4bfb6bb9f7ad67
Vladislav125 <myasoedovvladislav125@yandex.ru>
2022-12-28 19:19:48 +0300

    Final

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+function add_item(){
+    let tr = document.createElement('tr');
+    let td1 = document.createElement('td');
+    let td2 = document.createElement('td');
+    td1.innerHTML = document.getElementById('add_item')
+}
